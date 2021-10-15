eb4dce32303307d363dbf236d920c3b583cd9ffb
ferdvtn <hayferdian@gmail.com>
2021-10-15 22:47:19 +0700

    update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'main-portfolio',
+    title: 'Ahmad Ferdian',
     htmlAttrs: {
       lang: 'en'
     },
