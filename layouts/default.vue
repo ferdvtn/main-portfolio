658d616bfb99cde8a31ec9464fcb0ba571e004ef
ferdvtn <hayferdian@gmail.com>
2021-10-16 17:49:39 +0700

    update mode light footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen">
     <Nuxt />
     <footer
-      class="bg-gray-900 text-gray-300 mt-40 md:mt-60 py-3 md:py-6 px-3 text-xs text-center"
+      class="bg-gray-200 text-gray-800 dark:bg-gray-900 dark:text-gray-300 mt-40 md:mt-60 py-3 md:py-6 px-3 text-xs text-center"
     >
       <p class="leading-loose">Copyright &copy; 2021, Ahmad Ferdian<br>All rights reserved</p>
     </footer>
